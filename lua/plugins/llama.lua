74a85c058aa0a3ce1d93be67577bc487f9683b56
Dimitri Wulffert <dimknight@gmail.com>
2024-05-13 19:26:59 +0200

    Add AI support

---
+++ lua/plugins/llama.lua
@@ -0,0 +1,38 @@
+return {
+  "nomnivore/ollama.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+
+  -- All the user commands added by the plugin
+  cmd = { "Ollama", "OllamaModel", "OllamaServe", "OllamaServeStop" },
+
+  keys = {
+    -- Sample keybind for prompt menu. Note that the <c-u> is important for selections to work properly.
+    {
+      "<leader>oo",
+      ":<c-u>lua require('ollama').prompt()<cr>",
+      desc = "ollama prompt",
+      mode = { "n", "v" },
+    },
+
+    -- Sample keybind for direct prompting. Note that the <c-u> is important for selections to work properly.
+    {
+      "<leader>oG",
+      ":<c-u>lua require('ollama').prompt('Generate_Code')<cr>",
+      desc = "ollama Generate Code",
+      mode = { "n", "v" },
+    },
+  },
+
+  opts = {
+    model = "llama2",
+    prompts = {
+      Rate_Code = {
+        prompt = "From 0 to 10 rate the quality of follwing code:\n```$ftype\n$sel\n```",
+        input_label = "> ",
+        action = "display",
+      },
+    },
+  },
+}
