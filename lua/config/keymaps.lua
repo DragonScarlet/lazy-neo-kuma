0539a43b8ccd1d6dc35610375dc095feea39f78a
Dimitri Wulffert <dimknight@gmail.com>
2024-05-17 13:36:09 +0200

    Remove unused code

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,31 +1,12 @@
 local keymap = vim.keymap.set
-local command = vim.api.nvim_create_user_command
-
 vim.g.mapleader = " "
 
--- Exit
--- command("Qq", "q! | q! | q!", {})
-
 -- Autoformat
--- keymap("v", "<leader>f", ":'<, '> lua vim.lsp.buf.format() <CR>", { desc = "Format selection" })
 keymap("n", "<leader>F", ":lua vim.lsp.buf.format() <CR>", { desc = "File Format" })
 
 -- File
 keymap("n", "<leader>fc", ":%yank +<CR>", { desc = "File Copy" })
 keymap("n", "<leader>fp", ":%delete | put +<CR>", { desc = "File Paste" })
-
--- nvim-tree
--- keymap("n", "<leader>pt", "<cmd>NvimTreeToggle<CR>", { desc = "Project Tree" })
--- keymap("n", "<leader>pe", "<cmd>TroubleToggle<CR>", { desc = "Project Errors" })
-
--- Git
--- keymap("n", "<leader>gs", vim.cmd.Git, { desc = "Git Status" })
--- keymap("n", "<leader>gr", ":2,$s/pick/s<CR>", { desc = "Git Replace (Pick with S)" })
--- keymap("n", "<leader>gf", ":AdvancedGitSearch diff_commit_file<CR>", { desc = "Git File History" })
--- keymap("v", "<leader>gl", ":'<,'>AdvancedGitSearch diff_commit_line<CR>", { desc = "Git Line History" })
-
--- MSP
--- keymap("n", "<leader>kr", ":%delete | put +<CR>:%s/develop/acc<CR>:%s/dev/acc<CR>", { desc = "Kuma Replace" })
 
 -- Java
 keymap("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action" })
